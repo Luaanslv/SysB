afcd389ec1145f324b131e7731fdc7a5b08b7994
luaanslv <luaan.silva117@gmail.com>
2026-02-09 17:58:53 -0300

    feat(service/repository): implementando persistência de data de cadastro e último acesso

--- src/main/java/com/github/luaanslv/sysb/service/UsuarioService.java
+++ src/main/java/com/github/luaanslv/sysb/service/UsuarioService.java
@@ -1,4 +1,36 @@
 package com.github.luaanslv.sysb.service;
 
+import com.github.luaanslv.sysb.model.entitys.Usuario;
+import com.github.luaanslv.sysb.model.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
 public class UsuarioService {
+
+    private UsuarioRepository repository;
+
+    public UsuarioService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    public void salvarDtCadastro(String email, Date dtCadastro){
+        Usuario usuario = repository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Registro de cadastro não encontrado para o email: " + email));
+
+        usuario.setDtCadastro(dtCadastro);
+        repository.save(usuario);
+
+    }
+
+    public void salvarUltimoAcesso(String email, Date dtUltimoAcesso){
+        Usuario usuario = repository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Registro de ultimo acesso não encontrado para o email: " + email));
+
+        usuario.setDtUltimoAcesso(dtUltimoAcesso);
+        repository.save(usuario);
+    }
+
+
 }
